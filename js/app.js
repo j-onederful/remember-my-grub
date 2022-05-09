ea62f0dbbeb4cbb6b38ae13309a520bab08300f5
“j-onederfulgit config --global user.email “jackwwyman@gmail.com <jackwyman@macbook-pro-2.lan>
2022-05-09 16:47:45 -0700

    overlayed grid that holds buttons on top of new grid that will hold pictures

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// Constants
+const totalSeconds = 60;
+
+// DOM Refs
+const seconds = document.querySelector('#seconds')
+
+const timerCountdown = () => {
+    --totalSeconds
+    seconds.innerText = 
+}
