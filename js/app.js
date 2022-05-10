bfac0499771d30bba7957462e11ccc53b4ecfcc9
“j-onederfulgit config --global user.email “jackwwyman@gmail.com <jackwyman@macbook-pro-2.lan>
2022-05-10 14:27:16 -0700

    added timer function, need to format

--- js/app.js
+++ js/app.js
@@ -2,9 +2,36 @@
 const totalSeconds = 60;
 
 // DOM Refs
-const seconds = document.querySelector('#seconds')
+// const seconds = document.querySelector('#seconds')
 
-const timerCountdown = () => {
-    --totalSeconds
-    seconds.innerText = 
+// const timerCountdown = () => {
+//     --totalSeconds
+//     seconds.innerText = 
+// }
+
+var seconds = 60;
+let timer;
+function myFunction() {
+  if (seconds < 60) {
+    document.getElementById("timer").innerHTML = seconds;
+  }
+     if (seconds > 0 ) {
+         seconds--;
+     } else {
+         clearInterval(timer);
+         alert("Time's up champ");
+     }
 }
+document.getElementById("button").onclick = function() {
+  if(!timer) {
+    timer = window.setInterval(function() {
+      myFunction();
+    }, 1000);
+  }
+} 
+//When a key is pressed in the text area, update the timer using myFunction
+
+ //If seconds are equal or greater than 0, countdown until 1 minute has passed
+//Else, clear the timer and alert user of how many words they type per minute
+
+document.getElementById("timer").innerHTML="1:00";
