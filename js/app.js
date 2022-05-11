7bea79292c428ad112e6a149debe7f3995e58ce0
“j-onederfulgit config --global user.email “jackwwyman@gmail.com <jackwyman@macbook-pro-2.lan>
2022-05-11 15:38:27 -0700

    add files for deploy

--- js/app.js
+++ js/app.js
@@ -3,19 +3,35 @@
 const pictures = document.querySelectorAll('.hidden-pic')
 console.log(pictures[0])
 // const imgList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-const imgList = ['./img/avoToast.png', './img/backyardBurg.JPG', './img/beans.JPG', './img/BLT.JPG', './img/cheeseSteak.png', './img/clubSando.JPG', './img/creamPie.JPG', './img/pretzelBun.png', './img/spicyTuna.png', './img/steakAndEggs.jpg']
-const pushArr = []
+const imgList = ['./img/avoToast.png', './img/avoToast.png', './img/backyardBurg.JPG', './img/backyardBurg.JPG', './img/beans.JPG', './img/beans.JPG', './img/BLT.JPG', './img/BLT.JPG', './img/cheeseSteak.png', './img/cheeseSteak.png', './img/clubSando.JPG', './img/clubSando.JPG', './img/creamPie.JPG', './img/creamPie.JPG', './img/pretzelBun.png', './img/pretzelBun.png', './img/spicyTuna.png', './img/spicyTuna.png', './img/steakAndEggs.jpg', './img/steakAndEggs.jpg']
+
+
+
+
 
 // Add images
 function addPic(event) {
     const img = document.createElement("img")
-    //come up with random number on array
-    const num = Math.floor(Math.random() * imgList.length)
-    if (pushArr.indexOf(imgList[num]) != - 1) {
-        imgList.splice(num, 1)
-    }
-    pushArr.push(imgList[num])
-    console.log(imgList, pushArr)
+    const num = Math.floor(Math.random() * 10)
+    // console.log(imgList[num])
+    img.src = imgList[num]
+    img.style.height = '182px'
+    img.style.width = '182px'
+    const ele = event.target.lastElementChild
+
+    console.log(ele)
+    ele.appendChild(img)
+}
+pictures.forEach(element => element.addEventListener('click', addPic))
+
+
+
+
+// Add images
+function addPic(event) {
+    const img = document.createElement("img")
+    const num = Math.floor(Math.random() * 10)
+    // console.log(imgList[num])
     img.src = imgList[num]
     img.style.height = '182px'
     img.style.width = '182px'
