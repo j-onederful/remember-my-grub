c28ad0e7956dbcdc33ae8e811fda30a4cf3d59bb
“j-onederfulgit config --global user.email “jackwwyman@gmail.com <jackwyman@macbook-pro-2.lan>
2022-05-10 20:51:55 -0700

    almost done getting JS set up for picture entry, randomization and st yling. need to add pictures into imgList array

--- js/app.js
+++ js/app.js
@@ -1,37 +1,50 @@
 // Constants
-const totalSeconds = 60;
+//grab all divs (list or array of divs)
+const pictures = document.querySelectorAll('.hidden-pic')
+console.log(pictures[0])
+const imgList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-// DOM Refs
-// const seconds = document.querySelector('#seconds')
 
-// const timerCountdown = () => {
-//     --totalSeconds
-//     seconds.innerText = 
-// }
+// Add images
+function addPic(event) {
+    const img = document.createElement("img")
+    const num = Math.floor(Math.random() * 10)
+    console.log(imgList[num])
+    img.src = imgList[num]
+    img.style.height = 200
+    img.style.width = 200
+    const ele = event.target.lastElementChild
 
+    console.log(ele)
+    ele.appendChild(img)
+}
+// addPic()
+// pictures[0].addEventListener('click', addPic)
+pictures.forEach(element => element.addEventListener('click', addPic))
+
+
+//TIMER
 var seconds = 60;
 let timer;
 function myFunction() {
   if (seconds < 60) {
     document.getElementById("timer").innerHTML = seconds;
   }
+   //If seconds are equal or greater than 0, countdown until 1 minute has passed
      if (seconds > 0 ) {
          seconds--;
      } else {
          clearInterval(timer);
+         //Else, clear the timer and alert user that game is over
          alert("Time's up champ");
      }
 }
 document.getElementById("button").onclick = function() {
+    //When a button is clicked, update the timer using myFunction
   if(!timer) {
     timer = window.setInterval(function() {
       myFunction();
     }, 1000);
   }
 } 
-//When a key is pressed in the text area, update the timer using myFunction
-
- //If seconds are equal or greater than 0, countdown until 1 minute has passed
-//Else, clear the timer and alert user of how many words they type per minute
-
 document.getElementById("timer").innerHTML="1:00";
