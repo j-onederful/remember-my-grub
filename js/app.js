1a51765bb04449008f435f40f9455cd661968c2d
“j-onederfulgit config --global user.email “jackwwyman@gmail.com <jackwyman@macbook-pro-2.lan>
2022-05-11 18:22:57 -0700

    button stuff

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 // Constants
 //grab all divs (list or array of divs)
 const pictures = document.querySelectorAll('.hidden-pic')
+const buttons = document.querySelectorAll('.box')
 console.log(pictures[0])
-// const imgList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 const imgList = ['./img/avoToast.png', './img/avoToast.png', './img/backyardBurg.JPG', './img/backyardBurg.JPG', './img/beans.JPG', './img/beans.JPG', './img/BLT.JPG', './img/BLT.JPG', './img/cheeseSteak.png', './img/cheeseSteak.png', './img/clubSando.JPG', './img/clubSando.JPG', './img/creamPie.JPG', './img/creamPie.JPG', './img/pretzelBun.png', './img/pretzelBun.png', './img/spicyTuna.png', './img/spicyTuna.png', './img/steakAndEggs.jpg', './img/steakAndEggs.jpg']
+const buttonArray = []
 
 let randomList = imgList.sort(() => {
     return Math.random() - .5
@@ -14,9 +15,9 @@ console.log(randomList)
 
 
 // Add images
-function addPic(event) {
+// function addPic(event) {
     
-}
+// }
 pictures.forEach((element, index) => {
     const img = document.createElement("img")
     // console.log(imgList[num])
@@ -27,25 +28,65 @@ pictures.forEach((element, index) => {
     element.appendChild(img)
 })
 
+//Hide overlaying box buttons
+const box1 = document.getElementById('box-1')
+box1.addEventListener('click', hideElement.bind(this, box1))
+const box2 = document.getElementById('box-2')
+box2.addEventListener('click', hideElement.bind(this, box2))
+// const box3 = document.getElementById('box-3')
+// box3.addEventListener('click', hideElement.bind(this, box3))
+const box4 = document.getElementById('box-4')
+box4.addEventListener('click', hideElement.bind(this, box4))
+const box5 = document.getElementById('box-5')
+box5.addEventListener('click', hideElement.bind(this, box5))
+const box6 = document.getElementById('box-6')
+box6.addEventListener('click', hideElement.bind(this, box6))
+const box7 = document.getElementById('box-7')
+box7.addEventListener('click', hideElement.bind(this, box7))
+const box8 = document.getElementById('box-8')
+box8.addEventListener('click', hideElement.bind(this, box8))
+const box9 = document.getElementById('box-9')
+box9.addEventListener('click', hideElement.bind(this, box9))
+const box10 = document.getElementById('box-10')
+box10.addEventListener('click', hideElement.bind(this, box10))
+const box11 = document.getElementById('box-11')
+box11.addEventListener('click', hideElement.bind(this, box11))
+const box12 = document.getElementById('box-12')
+box12.addEventListener('click', hideElement.bind(this, box12))
+const box13 = document.getElementById('box-13')
+box13.addEventListener('click', hideElement.bind(this, box13))
+const box14 = document.getElementById('box-14')
+box14.addEventListener('click', hideElement.bind(this, box14))
+const box15 = document.getElementById('box-15')
+box15.addEventListener('click', hideElement.bind(this, box15))
+const box16 = document.getElementById('box-16')
+box16.addEventListener('click', hideElement.bind(this, box16))
+const box17 = document.getElementById('box-17')
+box17.addEventListener('click', hideElement.bind(this, box17))
+const box18 = document.getElementById('box-18')
+box18.addEventListener('click', hideElement.bind(this, box18))
+const box19 = document.getElementById('box-19')
+box19.addEventListener('click', hideElement.bind(this, box19))
+const box20 = document.getElementById('box-20')
+box20.addEventListener('click', hideElement.bind(this, box20))
+// const boxBtnList = document.querySelectorAll('.box')
+// boxBtnList.forEach(ele => ele.addEventListener('click', hideElement.bind(this, ele)))
+
+// function hide(a) {
+//     // hideElement(a);
+//     timeoutID = window.setTimeout(() => hideElement(a), 2000);
+//   }
+  
+  function hideElement() {
+    if (this.style.display === "none") {
+      this.style.display = "block";
+    } else {
+      this.style.display = "none";
+    }
+  
+  }
 
 
-
-// // Add images
-// function addPic(event) {
-//     const img = document.createElement("img")
-//     const num = Math.floor(Math.random() * 10)
-//     // console.log(imgList[num])
-//     img.src = imgList[num]
-//     img.style.height = '182px'
-//     img.style.width = '182px'
-//     const ele = event.target.lastElementChild
-
-//     console.log(ele)
-//     ele.appendChild(img)
-// }
-// // addPic()
-// // pictures[0].addEventListener('click', addPic)
-// pictures.forEach(element => element.addEventListener('click', addPic))
 
 
 
